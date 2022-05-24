d82550c81e21c866aab28ce46055735193933158
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-24 19:03:12 -0300

    Exemplo de como usar Omit e conclusão do curso

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import $ from 'jquery';
+interface Pessoa{
+    nome: string;
+    idade: number;
+    nacionalidade: string
+}
 
-$.fn.extend({
-    novaFuncao(){
-        console.log('Chamou nova funcao');
-    }
-});
+interface Brasileiro extends Omit<Pessoa, "nacionalidade">{
 
-$('body').novaFuncao();
+}
+
+const brasileiro: Brasileiro ={
+idade: 10,
+nome: 'joao'
+}
