713ea2c84fde698b671b435b07d42806c986fe36
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-24 17:07:43 -0300

    Tratando a tag input

--- index.ts
+++ index.ts
@@ -1,32 +1,5 @@
-/* function soma(a: number, b: number) {
-  return a + b;
-}
+const input = document.getElementById("input") as HTMLInputElement;
 
-soma(1, 2);
-
-//types
-//interfaces
-*/
-
-interface IAnimal {
-  nome: string;
-  tipo: "terrestre" | "aquático";
-  domestico: boolean;
-}
-
-interface IFelino extends IAnimal {
-  visaoNoturna: boolean;
-}
-
-interface ICanino extends IAnimal {
-  porte: "Pequeno" | "Medio" | "Grande";
-}
-
-type IDomestico = IFelino | ICanino;
-
-const animal: IDomestico = {
-  domestico: true,
-  nome: "Cachorro",
-  porte: "Medio",
-  tipo: "terrestre",
-};
+input.addEventListener("input", (event) => {
+  const i = event.currentTarget as HTMLInputElement;
+});
