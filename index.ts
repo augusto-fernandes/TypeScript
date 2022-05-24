9fdff4d48295d4418d277ba37e72e6a9aed105a6
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-24 17:14:53 -0300

    O que são Generic types

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-const input = document.getElementById("input") as HTMLInputElement;
+/* const input = document.getElementById("input") as HTMLInputElement;
 
 input.addEventListener("input", (event) => {
   const i = event.currentTarget as HTMLInputElement;
 });
+ */
+
+//Generic Types
+
+function AdicionaApendiceALista<T>(array: any[], valor: T){
+    return array.map(() => valor);
+}
+
+AdicionaApendiceALista([1, 2, 3],2);
